920756234d8bf66421f8fcbb253b66ea1559a1c5
Shouvik Basu <babanomania@gmail.com>
2025-07-20 13:23:39 +1000

    fix(dbt): truncate referencing tables with seed

--- dbt/macros/load_static_data.sql
+++ dbt/macros/load_static_data.sql
@@ -1,13 +1,13 @@
 {% macro load_static_data() %}
 insert into {{ target.schema }}.customers (name, email)
-select name, email from {{ ref('seed_customers') }}
+select name, email from {{ ref('customers') }}
 on conflict do nothing;
 
 insert into {{ target.schema }}.products (name, price)
-select name, price from {{ ref('seed_products') }}
+select name, price from {{ ref('products') }}
 on conflict do nothing;
 
 insert into {{ target.schema }}.employees (name, active)
-select name, active from {{ ref('seed_employees') }}
+select name, active from {{ ref('employees') }}
 on conflict do nothing;
 {% endmacro %}
